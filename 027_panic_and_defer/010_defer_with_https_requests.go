28cae5ec569deeb4029617aedac9704490f9a9a9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-09-09 14:09:25 +0300

    Task 34 : Revise Defer with https request example

--- 027_panic_and_defer/010_defer_with_https_requests.go
+++ 027_panic_and_defer/010_defer_with_https_requests.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -16,7 +16,7 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
